bc9413acd9d4ffb2aa63cb64658e0a9ac996c55f
thesua7 <sams.ahamed@northsouth.edu>
2023-05-16 08:22:36 +0600

    init

---
+++ taskOne.py
@@ -0,0 +1,116 @@
+import json
+import os
+from itertools import chain
+
+MainData={}
+annotation_objects = {}
+vehicle = {}
+license_plate = {}
+vehicleAttr={}
+licenseAttr={}
+annotation_attributes = {}
+
+def buildingStockStructure(nameofDataset):
+
+    
+    MainData['dataset_name'] = nameofDataset
+    MainData['image_link'] = None
+    MainData['annotation_type'] = "image"
+    vehicle['presence']=0
+    vehicle['bbox'] = []
+    annotation_objects['vehicle']=vehicle
+    MainData['annotation_objects'] = annotation_objects
+    vehicleAttr['Type']=None
+    vehicleAttr['Pose']=None
+    vehicleAttr['Model']=None
+    vehicleAttr['Make']=None
+    vehicleAttr['Color']=None
+    annotation_attributes['vehicle']=vehicleAttr
+    MainData['annotation_attributes'] = annotation_attributes
+    license_plate['presence']=0
+    license_plate['bbox'] = []
+    annotation_objects['license_plate']=license_plate
+    MainData['annotation_objects'] = annotation_objects
+    licenseAttr['Difficulty Score'] = None
+    licenseAttr['Value'] = None
+    licenseAttr['Occlusion'] =None
+    annotation_attributes['license_plate']=licenseAttr
+    MainData['annotation_attributes'] = annotation_attributes
+
+def generateJsonFromFile(nameofDataset,pathTemp):
+
+        fp = open(pathTemp, 'r+');
+
+        data = json.load(fp)
+        jtopy=json.dumps(data) 
+        dict_json=json.loads(jtopy) 
+
+
+
+
+
+        if len(dict_json['objects'])==0:
+            buildingStockStructure(nameofDataset)
+        else:
+            for i in data['objects']:
+             check =i['classTitle'] 
+             if check.__eq__("Vehicle"):
+                MainData['dataset_name'] = nameofDataset
+                MainData['image_link'] = None
+                MainData['annotation_type'] = "image"
+                vehicle['presence']=1
+                flatten_list = list(chain.from_iterable(i['points']['exterior']))
+                vehicle['bbox'] = flatten_list
+                annotation_objects['vehicle']=vehicle
+
+                MainData['annotation_objects'] = annotation_objects
+                
+                for each in i['tags']:
+                    vehicleAttr[each['name']]=each['value']
+
+                annotation_attributes['vehicle']=vehicleAttr
+                MainData['annotation_attributes'] = annotation_attributes
+
+
+            if  check.__eq__("License Plate"):
+                        MainData['dataset_name'] = nameofDataset
+                        MainData['image_link'] = ""
+                        MainData['annotation_type'] = "image"
+                        license_plate['presence']=1
+                        flatten_list = list(chain.from_iterable(i['points']['exterior']))
+                        license_plate['bbox'] = flatten_list
+                        annotation_objects['license_plate']=license_plate
+                        MainData['annotation_objects'] = annotation_objects
+                        for each in i['tags']:
+                            licenseAttr[each['name']]=each['value']
+                        
+                        licenseAttr['Occlusion'] =0
+                        annotation_attributes['license_plate']=licenseAttr
+                        MainData['annotation_attributes'] = annotation_attributes
+
+        print(MainData)
+        fileSavePlace = "\json_files"
+        # pathForSave = os.getcwd()+fileSavePlace
+        with open("json_files/test_formatted_"+nameofDataset, "w") as outfile:
+            json.dump(MainData, outfile)
+
+
+
+
+
+
+file0 = "\json_files\pos_0.png.json"
+path0 = os.getcwd()+file0
+
+datasetName0 = os.path.basename(path0)
+generateJsonFromFile(datasetName0,path0)
+
+file10010 = "\json_files\pos_10010.png.json"
+path10010 = os.getcwd()+file10010
+datasetName10010 = os.path.basename(path10010)
+generateJsonFromFile(datasetName10010,path10010)
+
+file10492 = "\json_files\pos_10492.png.json"
+path10492 = os.getcwd()+file10492
+datasetName10492 = os.path.basename(path10492)
+generateJsonFromFile(datasetName10492,path10492)
---
+++ taskTwo.py
@@ -0,0 +1,69 @@
+import json
+import os
+import pandas as pd
+from collections import defaultdict
+
+
+file1= "\json_files\pos_0.png.json"
+path1 = os.getcwd()+file1
+fp1 = open(path1, 'r+');
+data1 = json.load(fp1)
+
+
+file2= "\json_files\pos_10010.png.json"
+path2 = os.getcwd()+file2
+fp2 = open(path2, 'r+');
+data2 = json.load(fp2)
+
+file3= "\json_files\pos_10492.png.json"
+path3 = os.getcwd()+file3
+fp3 = open(path3, 'r+');
+data3 = json.load(fp3)
+
+
+def merge_dict(d1,d2, d3):        #took help from
+    dd = defaultdict(list)        #https://stackoverflow.com/questions/66059820/merge-two-json-object-in-python  
+    countV = 0
+    for d in (d1,d2, d3):
+
+        for key, value in d.items():
+            countV= countV+1
+          
+            if isinstance(value, list):
+                dd[key].extend(value)
+            else:
+                dd[key].append(value)
+
+    return dict(dd)
+
+
+json_str1 = json.dumps(data1)
+json_str2 = json.dumps(data2)
+json_str3 = json.dumps(data3)
+
+dct1 = json.loads(json_str1)
+dct2 = json.loads(json_str2)
+dct3 = json.loads(json_str3)
+
+combined_dct = merge_dict(dct1,dct2, dct3)
+
+allData = json.dumps(combined_dct)
+
+finalData = allData
+
+for i in combined_dct['objects']:
+    if i['classTitle'].__eq__("Vehicle"):
+        i['classTitle'] = "car"
+    
+    if  i['classTitle'].__eq__("License Plate"):
+        i['classTitle'] = "number"
+
+
+
+print(combined_dct)
+
+fileSavePlace = "\json_files"
+pathForSave = os.getcwd()+fileSavePlace
+with open("json_files/single_json.json","w") as outfile:
+    json.dump(combined_dct, outfile)
+
